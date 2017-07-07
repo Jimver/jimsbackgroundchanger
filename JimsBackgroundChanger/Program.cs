4f0e60db9ab1b670306fca71fff4863f79f3a8d3
Jimver <jim.verheijde@hotmail.com>
2017-07-07 20:41:27 +0200

    Add exception logger.

--- JimsBackgroundChanger/Program.cs
+++ JimsBackgroundChanger/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 using System.Windows.Forms;
 using Microsoft.Win32;
 
@@ -12,10 +13,21 @@ namespace JimsBackgroundChanger
         [STAThread]
         static void Main()
         {
+            AppDomain currentDomain = AppDomain.CurrentDomain;
+            currentDomain.UnhandledException += new UnhandledExceptionEventHandler(MyHandler);
             SystemEvents.DisplaySettingsChanged += Wallpaper.SystemEvents_DisplaySettingsChanged;
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form());
+        }
+
+        static void MyHandler(object sender, UnhandledExceptionEventArgs args)
+        {
+            Exception e = (Exception)args.ExceptionObject;
+            string msg = "MyHandler caught : " + e.Message + Environment.NewLine;
+            msg += $"Runtime terminating: {args.IsTerminating}";
+            Console.WriteLine(msg);
+            File.AppendAllText(@"C:\path\jbclog.txt", msg + Environment.NewLine);
         }
     }
 }
